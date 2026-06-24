0f735aa031eefdb9a28f566c1390de7907cdf041
KRZmishanya <ilinmv07@gmail.com>
2026-06-24 07:19:44 +0300

    create venv with requirements

--- index.py
+++ index.py
@@ -1,13 +1,16 @@
-#import numpy as np
-#A = np.array([[1, 2], [3, 4]])
-#B = np.array([[5, 6], [7, 8]])
-#C = A + B
-#print(C)
+# rg = np.random.default_rng(1)
+# a = np.array(10* rg.random(12)).reshape(3,4)
+# time = np.linspace(0,4,5)
+# ind = np.argmax(a,axis=0)
+# print(a)
+# print(ind)
+# a_max = a[ind, range(a.shape[1])]
+# print(np.all(a_max == a.max(axis=0)))
+#================Some numpy & pandas=================#
 import numpy as np
+import pandas as pd
+import matplotlib as plt
 
-a = np.ones((3, 4),dtype = int)
-b = np.arange(12).reshape((4, 3))
-print(a)
-print(b)
-c = np.dot(a,b)
-print(c)
+def main():
+    time = np.linspace(0,4,5)
+    print(time)
---
+++ kaka.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+time = np.linspace(0,4,5)
+print(time)
